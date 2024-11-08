6049232d015bed8992d459b7940d6bbd1d0df236
theepaksoni <git@stackblitz.com>
2024-11-08 09:27:51 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,333 @@
+const express = require('express');
+const { resolve } = require('path');
+let cors = require('cors');
+
+const app = express();
+const port = 3010;
+
+app.use(express.static('static'));
+app.use(cors());
+
+let products = [
+  {
+    id: 1,
+    name: 'Xiaomi iPhone 12',
+    brand: 'Xiaomi',
+    price: 60000,
+    ram: 6,
+    rom: 256,
+    rating: 4.5,
+    os: 'Android',
+    camera: 108,
+  },
+  {
+    id: 2,
+    name: 'Oppo Mi 10',
+    brand: 'Xiaomi',
+    price: 30000,
+    ram: 6,
+    rom: 512,
+    rating: 4,
+    os: 'iOS',
+    camera: 64,
+  },
+  {
+    id: 3,
+    name: 'Samsung Mi 10',
+    brand: 'Oppo',
+    price: 20000,
+    ram: 4,
+    rom: 256,
+    rating: 4,
+    os: 'Android',
+    camera: 24,
+  },
+  {
+    id: 4,
+    name: 'Apple Find X2',
+    brand: 'Samsung',
+    price: 60000,
+    ram: 8,
+    rom: 512,
+    rating: 4.5,
+    os: 'iOS',
+    camera: 48,
+  },
+  {
+    id: 5,
+    name: 'Oppo Mi 11',
+    brand: 'Xiaomi',
+    price: 30000,
+    ram: 12,
+    rom: 128,
+    rating: 4,
+    os: 'iOS',
+    camera: 24,
+  },
+  {
+    id: 6,
+    name: 'OnePlus Find X3',
+    brand: 'Apple',
+    price: 30000,
+    ram: 12,
+    rom: 64,
+    rating: 4,
+    os: 'Android',
+    camera: 64,
+  },
+  {
+    id: 7,
+    name: 'Apple Pixel 5',
+    brand: 'Apple',
+    price: 70000,
+    ram: 4,
+    rom: 512,
+    rating: 4.5,
+    os: 'iOS',
+    camera: 24,
+  },
+  {
+    id: 8,
+    name: 'Google Mi 10',
+    brand: 'Oppo',
+    price: 30000,
+    ram: 8,
+    rom: 64,
+    rating: 5,
+    os: 'iOS',
+    camera: 108,
+  },
+  {
+    id: 9,
+    name: 'Oppo Mi 11',
+    brand: 'Samsung',
+    price: 30000,
+    ram: 4,
+    rom: 64,
+    rating: 4,
+    os: 'Android',
+    camera: 24,
+  },
+  {
+    id: 10,
+    name: 'Xiaomi Mi 10',
+    brand: 'Oppo',
+    price: 60000,
+    ram: 16,
+    rom: 512,
+    rating: 4.5,
+    os: 'Android',
+    camera: 12,
+  },
+  {
+    id: 11,
+    name: 'OnePlus Pixel 5',
+    brand: 'Apple',
+    price: 60000,
+    ram: 12,
+    rom: 64,
+    rating: 5,
+    os: 'Android',
+    camera: 12,
+  },
+  {
+    id: 12,
+    name: 'Xiaomi OnePlus 8',
+    brand: 'Xiaomi',
+    price: 70000,
+    ram: 8,
+    rom: 64,
+    rating: 4.5,
+    os: 'Android',
+    camera: 48,
+  },
+  {
+    id: 13,
+    name: 'Xiaomi Pixel 6',
+    brand: 'Oppo',
+    price: 30000,
+    ram: 4,
+    rom: 64,
+    rating: 5,
+    os: 'Android',
+    camera: 108,
+  },
+  {
+    id: 14,
+    name: 'Samsung Find X2',
+    brand: 'Oppo',
+    price: 40000,
+    ram: 12,
+    rom: 512,
+    rating: 4.7,
+    os: 'Android',
+    camera: 48,
+  },
+  {
+    id: 15,
+    name: 'Google OnePlus 8',
+    brand: 'Apple',
+    price: 20000,
+    ram: 16,
+    rom: 64,
+    rating: 5,
+    os: 'iOS',
+    camera: 24,
+  },
+  {
+    id: 16,
+    name: 'OnePlus iPhone 12',
+    brand: 'OnePlus',
+    price: 20000,
+    ram: 6,
+    rom: 128,
+    rating: 4.5,
+    os: 'iOS',
+    camera: 64,
+  },
+  {
+    id: 17,
+    name: 'Google Mi 11',
+    brand: 'Oppo',
+    price: 70000,
+    ram: 6,
+    rom: 64,
+    rating: 4,
+    os: 'Android',
+    camera: 64,
+  },
+  {
+    id: 18,
+    name: 'Google OnePlus 9',
+    brand: 'Apple',
+    price: 20000,
+    ram: 4,
+    rom: 64,
+    rating: 5,
+    os: 'Android',
+    camera: 64,
+  },
+  {
+    id: 19,
+    name: 'Oppo Galaxy S22',
+    brand: 'Samsung',
+    price: 20000,
+    ram: 16,
+    rom: 256,
+    rating: 4.7,
+    os: 'Android',
+    camera: 12,
+  },
+  {
+    id: 20,
+    name: 'Apple Pixel 5',
+    brand: 'Oppo',
+    price: 40000,
+    ram: 8,
+    rom: 128,
+    rating: 4.7,
+    os: 'Android',
+    camera: 108,
+  },
+];
+
+app.get('/', (req, res) => {
+  res.send('Application Flip Deal Product Listing Page');
+});
+
+function sortedByPopularity(product1, product2) {
+  return product2.rating - product1.rating;
+}
+
+app.get('/products/sort/popularity', (req, res) => {
+  let sortedProducts = products.slice();
+  sortedProducts.sort(sortedByPopularity);
+  res.json(sortedProducts);
+});
+
+function sortedByPrice(product1, product2) {
+  return product2.price - product1.price;
+}
+app.get('/products/sort/price-high-to-low', (req, res) => {
+  let sortedProducts = products.slice();
+  sortedProducts.sort(sortedByPriceHighToLow);
+  res.json(sortedProducts);
+});
+
+function sortedByPriceLowToHigh(product1, product2) {
+  return product1.price - product2.price;
+}
+
+app.get('/products/sort/price-low-to-high', (req, res) => {
+  let sortedProducts = products.slice();
+  sortedProducts.sort(sortedByPriceLowToHigh);
+  res.json(sortedProducts);
+});
+
+function filterByRam(askedRam, product) {
+  return (product.ram = askedRam);
+}
+
+app.get('/products/filter/ram', (req, res) => {
+  let askedRam = req.query.ram;
+  let sortedProducts = products.filter((product) =>
+    filterByRam(askedRam, product)
+  );
+  res.json(sortedProducts);
+});
+
+function filterByRom(askedRom, product) {
+  return (product.rom = askedRom);
+}
+
+app.get('/products/filter/rom', (req, res) => {
+  let askedRom = req.query.rom;
+  let sortedProducts = products.filter((product) =>
+    filterByRom(askedRom, product)
+  );
+  res.json(sortedProducts);
+});
+
+function filterByBrand(askedBrand, product) {
+  return product.brand.toLowerCase() === askedBrand.toLowerCase();
+}
+
+app.get('/products/filter/brand', (req, res) => {
+  let askedBrand = req.query.brand;
+  let sortedProducts = products.filter((product) =>
+    filterByBrand(askedBrand, product)
+  );
+  res.json(sortedProducts);
+});
+
+function filterByOs(askedOs, product) {
+  return product.os.toLowerCase() === askedOs.toLowerCase();
+}
+
+app.get('/products/filter/os', (req, res) => {
+  let askedOs = req.query.os;
+  let sortedProducts = products.filter((product) =>
+    filterByOs(askedOs, product)
+  );
+  res.json(sortedProducts);
+});
+
+function filterByPrice(askedPrice, product) {
+  return product.price === askedPrice;
+}
+
+app.get('/products/filter/price', (req, res) => {
+  let askedPrice = parseFloat(req.query.price);
+  let sortedProducts = products.filter((product) =>
+    filterByPrice(askedPrice, product)
+  );
+  res.json(sortedProducts);
+});
+
+app.get('/products', (req, res) => {
+  res.json(products);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
